a015f2f03442e04649cd99f7fcff82032eebe923
Cristian Merli <stesvis@gmail.com>
2023-01-22 15:22:08 -0700

    Https only in production

--- Program.cs
+++ Program.cs
@@ -23,7 +23,10 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 app.UseStaticFiles();
 app.UseRouting();
 
